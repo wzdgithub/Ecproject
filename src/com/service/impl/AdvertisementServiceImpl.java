e4cde30832a7db3ea171b1d448173e18b1fc6dca
yuanguangxin <274841922@qq.com>
2016-03-05 18:56:32 +0800

    modify table

--- src/com/service/impl/AdvertisementServiceImpl.java
+++ src/com/service/impl/AdvertisementServiceImpl.java
@@ -3,4 +3,7 @@ package com.service.impl;
 import com.service.AdvertisementService;
 
 public class AdvertisementServiceImpl implements AdvertisementService{
+    public void test(){
+        System.out.println("dd");
+    }
 }
